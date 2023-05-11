1979c79814e7fc2624fb9108357cf02a633baa5b
Mariem Padron <mjpadron11@gmail.com>
2023-05-11 11:14:15 -0500

    Added side nav bar styles

--- src/data.js
+++ src/data.js
@@ -1,9 +1,22 @@
-// estas funciones son de ejemplo
+// export const mostViewed = () => {
+//   const allPosters = data.films.map(movies => movies.poster)
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//   const posters = allPosters.slice(0, 5)
+//   //Usar .sort cuando logremos traer los viewed con las imágenes
+
+//   const images = posters.map(src => {
+//     const img = document.createElement("img")
+//     img.src = src
+//     img.classList.add("gallery__img")
+//     return img
+//   })
+
+//   for (let i = 0; i <= images.length ; i++) {
+//     gallery.appendChild(images[i])
+//   }
+// };
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };

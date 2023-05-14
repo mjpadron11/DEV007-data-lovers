6b6eabde9b5c877bf0d35102988aa43d111ef7c9
IsaMonsalves <ester.isabella.m@gmail.com>
2023-05-13 21:44:13 -0400

    carrusel1

--- src/data.js
+++ src/data.js
@@ -9,25 +9,26 @@ export const anotherExample = () => {
   return "OMG";
 };
 
-const big = document.querySelector(".big");
-const point = document.querySelectorAll(".point");
-//lleva el all porque son varos a diferencia de big que solo es uno
-// se recorre todos los puntos para averiguar la posición en la que se presionar el punto, eso es con let posicion
-point.forEach((cadaPunto, i) => {
-  point[i].addEventListener("click", () => {
-    let position = i;
-    //recordar que i 0 es 0% y posicion 1  es 20%
-    //la sgtte operación da el numero exacto en el eje x a traves de traslate
-    let operation = position * -20;
-    big.style.transform = `translateX(${operation}%)`;
-    //con la linea anterior ya se desplaza al dar click a los puntos
-    //ahora se recorren los puntos para dar la caracteristica de activo(cambia color)
-    point.forEach((cadaPunto, i) => {
-      point[i].classList.remove("active");
-    });
-    //el sgt añade la clase actiivo al punto presionado
-    point[i].classList.add("active");
-  });
+var moviesSlider = new Swiper(".movies-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 //   const posters = allPosters.slice(0, 5)
@@ -45,6 +46,6 @@ point.forEach((cadaPunto, i) => {
 //   }
 // };
 
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
+export const anotherExample = () => {
+  return "OMG";
+};

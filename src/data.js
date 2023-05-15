c62aa22eed6a6444bbf105f748b1d91601e9ccd2
IsaMonsalves <ester.isabella.m@gmail.com>
2023-05-15 10:12:21 -0400

    commit fds

--- src/data.js
+++ src/data.js
@@ -9,28 +9,6 @@ export const anotherExample = () => {
   return "OMG";
 };
 
-var moviesSlider = new Swiper(".movies-slider", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  loop: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2.5,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
 //   const posters = allPosters.slice(0, 5)
 //   //Usar .sort cuando logremos traer los viewed con las imágenes
 
--- src/main.js
+++ src/main.js
@@ -5,3 +5,25 @@
 // mostViewed()
 
 // console.log(data);
+
+var moviesSlider = new Swiper(".movies-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 25,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});

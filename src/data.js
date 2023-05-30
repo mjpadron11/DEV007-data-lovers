a4fe4e550acd0ccc33d6d80d101b9b72a8d6b1b7
Mariem Padron <mjpadron11@gmail.com>
2023-05-30 12:17:41 -0500

    adding alfabetic, year and score filters

--- src/data.js
+++ src/data.js
@@ -1,8 +1,12 @@
 import data from "./data/ghibli/ghibli.js";
 const allMovies = document.getElementById("allMovies");
+const gallery = document.getElementById("gallery");
+const text = document.getElementById("search-input").value;
+const idDirector = document.getElementById("directores");
+const select = document.getElementById("filters")
+
 
 export const mostViewed = () => {
-  const gallery = document.getElementById("gallery");
   const allPosters = data.films.filter((movies) => movies.most_viewed);
 
   const images = allPosters.map((item) => {
@@ -13,6 +17,7 @@ export const mostViewed = () => {
     img.classList.add("gallery__img");
     p.classList.add("gallery__title");
     img.src = item.poster;
+    img.alt = item.title;
     p.textContent = item.description;
 
     div.appendChild(img);
@@ -29,8 +34,11 @@ export const movies = () => {
   //esto deja un array de string por lo que se crea una etiqueta imagen para poder insertar el poster
   const imagesMovies = data.films.map((item) => {
     const img = document.createElement("img");
+    // const div = document.createElement("div");
+    // div.classList.add("posters-container");
     img.classList.add("posters");
     img.src = item.poster;
+    img.alt = item.title;
     img.width = 100;
     img.heigth = 100;
     //return finaliza el callback
@@ -40,7 +48,6 @@ export const movies = () => {
 };
 
 export const searchMovies = () => {
-  const text = document.getElementById("search-input").value;
   const searchedMovies = data.films.filter((item) =>
     //se cambia el star with por includes(busca si está en el array y da un boolean)
     item.title.toLowerCase().includes(text.toLowerCase())
@@ -50,6 +57,7 @@ export const searchMovies = () => {
     const img = document.createElement("img");
     img.classList.add("posters");
     img.src = item.poster;
+    img.alt = item.title;
     img.width = 100;
     img.heigth = 100;
     //return finaliza el callback
@@ -59,63 +67,55 @@ export const searchMovies = () => {
   imagesMovies.map((image) => allMovies.appendChild(image));
 };
 
+const _forceRender = (valueToRender) => {
+  if (!valueToRender) return
+
+  const imagesMovies = valueToRender.map((item) => {
+    const img = document.createElement("img");
+    img.classList.add("posters");
+    img.src = item.poster;
+    img.width = 100;
+    img.heigth = 100;
+
+    return img;
+  });
+
+  allMovies.innerHTML = "";
+  imagesMovies.map((image) => allMovies.appendChild(image));
+}
+
 export const searchMoviesByDirector = () => {
-  const idDirector = document.getElementById("directores");
   const selectedDirector = idDirector.options[idDirector.selectedIndex].value;
 
   const searchMoviesByDirector = data.films.filter(
     (item) => item.director === selectedDirector
   );
 
-  console.log(searchMoviesByDirector);
-  const imagesMovies = searchMoviesByDirector.map((item) => {
-    const img = document.createElement("img");
-    img.classList.add("posters");
-    img.src = item.poster;
-    img.width = 100;
-    img.heigth = 100;
-    //return finaliza el callback
-    return img;
-  });
-  allMovies.innerHTML = "";
-  imagesMovies.map((image) => allMovies.appendChild(image));
+  _forceRender(searchMoviesByDirector)
 };
 
-// export const orderMovies = () => {
-//   let title = document.getElementById("allMovies").value;
 
-//   let orderdMovies = data.films.sort((item) =>
-//     item.title.toLowerCase().sort(text.toLowerCase())
-//   );
-//   const imagesMovies = title.map((item) => {
-//     const img = document.createElement("img");
-//     img.classList.add("posters");
-//     img.src = item.poster;
-//     img.width = 100;
-//     img.heigth = 100;
-//     //return finaliza el callback
-//     return img;
-//   });
-//   allMovies.innerHTML = "";
-//   imagesMovies.map((image) => allMovies.appendChild(image));
-// };
+export const sortByYear = () => {
+  const value = select.options[select.selectedIndex].value
 
-// for (let i = 0; i <= images.length ; i++) {
-//   allMovies.appendChild(images[i])
-// }
+  const films = data.films
 
-// export const showMovies = () => {
-//   data.forEach(movie => {
-//     const movieDiv = document.createElement("div");
-//     const movieTitle = document.createElement("h2");
-//     const movieImage = document.createElement("img");
+  if (value === "decendant") {
+    films.sort((a, b) => a.title.localeCompare(b.title))
+  }
 
-//     movieTitle.textContent = movie.title;
-//     movieImage.src = movie.poster;
+  if (value === "ascendant") {
+    films.sort((a, b) => b.title.localeCompare(a.title))
+  }
 
-//     movieDiv.appendChild(movieTitle);
-//     movieDiv.appendChild(movieImage);
+  if (value === "year") {
+    films.sort((a, b) => Number(a.release_date) - Number(b.release_date))
+  }
 
-//     allMovies.appendChild(movieDiv)
-//   })
-// }
+  if (value === "rt-score") {
+    films.sort((a, b) => Number(b.rt_score) - Number(a.rt_score))
+    console.log(films.sort((a, b) => Number(b.rt_score) - Number(a.rt_score)))
+  }
+
+  _forceRender(films)
+}
